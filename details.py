046b59a082dc3f7304b5aa16e2c3a9fceb34ebec
Ina Cecoi Sorocian <144531490+cecoii1987@users.noreply.github.com>
2024-06-20 12:12:36 +0100

    Add files via upload

---
+++ details.py
@@ -0,0 +1,9 @@
+# we will generate a variable for each input
+
+name = input('Please enter your name: ')
+age = input('Please enter your age: ')
+house_num = input('Please enter your house number: ')
+street_name = input('Please enter your street name: ')
+
+# The we will print out the unique statement
+print(f'This is {name}. He is {age} years old and lives at house number {house_num} on {street_name}')
---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# First of all we will start by defining the variable which in this case will be name
+
+name = input('Please enter your name: ')
+print(name)
+
+# The second part is asking for requesting the age of the user
+# Age can be considered as a string only for reading pruposes or as int if we will use to calculate
+# In this case we will use directly input as per the exercise request
+age = input('Please enter your age: ')
+print(age)
+
+# And the fird thing is asking is to print Hello World! in a new line
+
+print('Hello World!')
