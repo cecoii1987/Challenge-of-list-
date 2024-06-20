5593b5aadaa1073a3d7937b6ae3de90f12f47ec0
Ina Cecoi Sorocian <144531490+cecoii1987@users.noreply.github.com>
2024-06-20 15:45:48 +0100

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,12 @@
+# We will first create a variable called string that will contain the sentence
+string = 'The!quick!brown!fox!jumps!over!the!lazy!dog.'
+
+# We will create a variable with replace function and then print the new variable
+string1 = string.replace('!', ' ')
+print(string1)
+
+# Next we will reprint the new sentence with upper() function
+print(string1.upper())
+
+# Then is asking us to print the sentence in reverse
+print(string[::-1])
