3f72c0206a6d5da4c30431fcbc4728323336fb53
Ina Cecoi Sorocian <144531490+cecoii1987@users.noreply.github.com>
2024-06-20 12:34:52 +0100

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,12 @@
+# We already have the variable defined 
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = '100'
+
+# Now we will print al the variables converted
+print(int(num1))
+print(float(num2))
+print(str(num3))
+print(int(string1))
